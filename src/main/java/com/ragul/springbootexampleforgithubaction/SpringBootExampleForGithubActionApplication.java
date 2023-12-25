c78d7984abdbb53a606034723ef47e88f5647dd7
rragul <r.ragul0598@gmail.com>
2023-12-25 18:47:29 +0530

    Initial commit

---
+++ src/main/java/com/ragul/springbootexampleforgithubaction/SpringBootExampleForGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.ragul.springbootexampleforgithubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleForGithubActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExampleForGithubActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ragul/springbootexampleforgithubaction/SpringBootExampleForGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragul.springbootexampleforgithubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleForGithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
